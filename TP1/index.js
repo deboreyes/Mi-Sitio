1b12d239cedf98683978eb385a986b9ed1a592db
Georgina <georgimeli@gmail.com>
2018-04-16 18:52:11 -0300

    Slider final

---
+++ TP1/index.js
@@ -0,0 +1,64 @@
+var photos = ["imgs/slider1.jpg", "imgs/slider6.jpg", "imgs/slider7.jpg","imgs/slider5.png"]
+var numberPhotos = photos.length;
+var gallery = document.getElementById('gallery');
+var imgs = [];
+var dotList = [];
+var currentPhoto = 0;
+var dots = document.getElementById('dots');
+var timeout = null;
+
+// Función que carga las imágenes y los dots con su respectiva funcionabilidad.
+function loadSlider() {
+  for (let k = 0; k < numberPhotos; k++ ) {
+    var img = document.createElement('img');
+    var dot = document.createElement('li');
+    img.src = photos[k];
+    imgs.push(img);
+    dot.classList.add("dot");
+    dotList.push(dot);
+    gallery.appendChild(img);
+    dots.appendChild(dot);
+    //Funcconabilidad dot.
+    dot.onclick = function() {
+      setCurrentPhoto(k)
+    }
+  }
+  //funcionabilidad gallery.
+  gallery.onclick = function() {
+    setCurrentPhoto((currentPhoto + 1) % numberPhotos);
+  }
+}
+
+// Función que establece qué foto se muestra y activa el dot correspondiente.
+function setCurrentPhoto(index) {
+  currentPhoto = index;
+  for (var j = 0; j < numberPhotos; j++) {
+    dotList[j].classList.remove("active");
+    imgs[j].style.display = "none";
+  }
+  imgs[currentPhoto].style.display = "block";
+  dotList[currentPhoto].classList.add("active");
+  if (timeout) clearTimeout(timeout);
+  timeout = setTimeout(gallery.onclick, 2000);
+}
+
+(function () {
+  gallery.addEventListener('mouseover', function () {
+    var caption = document.getElementById('captions');
+    caption.style.display = "block";
+  })
+  gallery.addEventListener('mouseout', function () {
+    var caption = document.getElementById('captions');
+    caption.style.display = "none";
+  })
+
+})()
+
+
+
+
+loadSlider();
+setCurrentPhoto(0);
+
+//Uso let en lugar de var para solucionar el problema del scope en Js. IMP: no anda en todos los browser.
+//let es similar a var pero tiene scope por bloques: la variable se define y muere en la apertura y cierre de llaves.
